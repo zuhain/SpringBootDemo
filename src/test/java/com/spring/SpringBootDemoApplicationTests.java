39cd75a23256fccc132172855e7c5689ca8c37dd
zuhain <34508556+zuhain@users.noreply.github.com>
2017-12-19 16:21:41 +0530

    added i18n to the project

--- src/test/java/com/spring/SpringBootDemoApplicationTests.java
+++ src/test/java/com/spring/SpringBootDemoApplicationTests.java
@@ -2,15 +2,27 @@ package com.spring;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.spring.web.i18n.I18NService;
+
+import junit.framework.Assert;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootDemoApplicationTests {
 
+	@Autowired
+	I18NService i18nService;
+	
 	@Test
-	public void contextLoads() {
+	public void testMessageByLocaleService() throws Exception{
+		String expectedResult="Bootstrap starter template";
+		String messageId="index.title";
+		String actual=i18nService.getMessage(messageId);
+		Assert.assertEquals("actual and expected Strings dont match", expectedResult, actual);
 	}
 
 }
