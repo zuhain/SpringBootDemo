dcbf3e230fe119f0d5268c318a89fa08831a1f5e
zuhain <34508556+zuhain@users.noreply.github.com>
2017-12-20 15:17:02 +0530

    added about us page

---
+++ src/main/java/com/spring/web/controllers/CopyController.java
@@ -0,0 +1,13 @@
+package com.spring.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CopyController {
+	
+	@RequestMapping("/about")
+	public String about() {
+		return "copy/about";
+	}
+}
